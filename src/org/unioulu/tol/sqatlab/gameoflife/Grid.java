f463d27938da89f2357e4845163e184c2ed0cdab
somename <someemail>
2015-10-08 13:17:43 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -4,12 +4,13 @@ public class Grid {
 	//GRID X * Y 
 	static int gridSize=8;
 	
-	public static int[][]array = new int [gridSize][gridSize];
+	public static String[][]array = new String [gridSize][gridSize];
 
 	
 	
 	
-public static int getArray(){
+public static int getArray(){ 
+	// gets the row count of the array
 	 int a =	array.length;
 	 return a;
 	}
