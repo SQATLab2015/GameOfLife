e18776b573cb9ff16d043e445c56ea9e80ac189c
somename <someemail>
2015-10-08 13:32:00 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -28,9 +28,11 @@ public class TestGrid {
 	private Object getMarks() {
 		//Array to string list
 		//check if only * and - then return true
-	Grid.getArrayAsList().matches("*-");
-		
+	if (Grid.getArrayAsList().matches("*-")){
 		return true;
+	} else return false;
+		
+		
 	}
 
 }
