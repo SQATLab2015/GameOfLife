547a04bf3db781149274673b2ebbb79cae8d239e
somename <someemail>
2015-10-08 12:10:27 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -2,9 +2,21 @@ package org.unioulu.tol.sqatlab.gameoflife.test;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.unioulu.tol.sqatlab.gameoflife.Cell;
+import org.unioulu.tol.sqatlab.gameoflife.Grid;
 
 public class TestGrid {
 
+	private Grid testGrid = new Grid();
+	
+	//Initialization function
+	public void initializeGrid() {
+		testGrid = new Grid();
+	}
+	
 	@Test
+	public void test_createA10By10Grid () {
+		
+	}
 	
 }
