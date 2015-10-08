098c8343f505e95f334b89dc37a7a6a0956cbfe4
somename <someemail>
2015-10-08 12:24:58 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 
 public class Grid {
 
-	private static ArrayList<Integer> gridArray = new ArrayList<Integer>();
+	private static ArrayList[][] gridArray = new ArrayList[0][0];
 	
 	public Grid(int width, int height) {
+		int i = 0;
+		int b = 0;
+		
+		while ( i < width) {
+			while ( b < height ) {
+				gridArray[i][b].add(0);
+				b++;
+			}
+			i++;
+		}
 		
 	}
+	
+	
 }
