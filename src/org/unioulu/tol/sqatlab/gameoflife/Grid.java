c93384dda3d9d81771a95cf961ac77bd99c00d20
somename <someemail>
2015-10-08 12:51:58 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -2,14 +2,13 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 import java.util.List;
 
-import fi.oulu.tol.sqat.Item;
 
 public class Grid {
 	
 	public int rowAmount;
 	public int columnAmount;
 	
-	public List<Cell> cells = null;
+	public List<List<Cell>> cells = null;
 	
 	
 	public void createGrid()
