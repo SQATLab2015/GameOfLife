142106402bc9850cb7e2c04c3026d2d639b11e91
somename <someemail>
2015-10-08 12:58:55 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
@@ -1,5 +1,13 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
 public class GameOfLife {
+	
+	private static Boolean running = true;
+	
+	public void main () {
+		while (running) {
+			running = false;
+		}
+	}
 
 }
