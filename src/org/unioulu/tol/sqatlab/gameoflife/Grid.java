2640472f3be5cf65d6a7e9525d7b4a7e3c6d2dab
thomaspetry <tpetry@student.oulu.fi>
2015-10-08 13:48:01 +0300

    testCommit

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,14 +1,46 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Grid {
-	public int[][] grid = new int[9][9];
+	Cell zelle = new Cell();
+	public Cell[][] grid = new Cell[9][9];
 	
 	public void start() {
 		for(int i = 0; i < grid.length; i++) {
 			for(int j = 0; j < grid.length; j++) {
-				Cell(i,j);
+				zelle.cell(i,j);
 			}
 		}
+	}
+	
+	public void neighboursetting(int x, int y){
+		int temp = 0;
+		if (grid[x-1][y-1].getState() == "*") {
+			temp++;
+		}
+		if (grid[x][y-1].getState() == "*") {
+			temp++;
+		}
+		if (grid[x+1][y-1].getState() == "*") {
+			temp++;
+		}
+		if (grid[x-1][y].getState() == "*") {
+			temp++;
+		}
+		if (grid[x+1][y].getState() == "*") {
+			temp++;
+		}
+		if (grid[x-1][y+1].getState() == "*") {
+			temp++;
+		}
+		if (grid[x][y+1].getState() == "*") {
+			temp++;
+		}
+		if (grid[x+1][y+1].getState() == "*") {
+			temp++;
+		}
+		grid[x][y].setNeighbour(temp);
+		
+			
 	}
 
 	public void update() {
