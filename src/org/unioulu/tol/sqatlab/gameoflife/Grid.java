1e6365ae12b808835a8635654832b4829528af9c
somename <someemail>
2015-10-08 13:47:10 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -10,6 +10,7 @@ public static void setCharacters(){
 	for(int i =0; i< array.length; i++){//cols
 		for(int k =0; k< array.length; k++){//rows
 		 array[i][k]="-";
+		 System.out.Println(array[i][k]);
 		}	
 	}
 
