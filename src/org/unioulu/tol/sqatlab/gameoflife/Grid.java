a82e1d0091f1388294db4325d898afa1df2039f2
somename <someemail>
2015-10-08 12:19:51 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,5 +1,12 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
+import java.util.ArrayList;
+
 public class Grid {
 
+	private static ArrayList<Integer> gridArray = new ArrayList<Integer>();
+	
+	public Grid(int width, int height) {
+		
+	}
 }
