226019371e364cb661b6eaf877eebb46c4a3b623
somename <someemail>
2015-10-08 12:17:34 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -3,6 +3,7 @@ package org.unioulu.tol.sqatlab.gameoflife;
 public class Grid {
 
 	public static final int NEIGHBOR_STATE_LESS_THAN_TWO = 0;
+	public static final int NEIGHBOR_STATE_TWO_OR_THREE = 1;
 	
 	private int size;
 	
