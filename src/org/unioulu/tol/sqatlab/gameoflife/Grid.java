052faf508dc7b813018f7219f00291edac0a904f
somename <someemail>
2015-10-08 13:33:00 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,12 +1,16 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
+import org.univoulu.tol.sqatlab.sudoku.SudokuVerifier;
+
 public class Grid {
-     int [][] grid = new int [9][9];
+	Cell zelle =new Cell();
+	
+   public Cell [][] grid = new Cell [9][9];
      
      public void start() {
     	 for(int i = 0; i < grid.length; i++)
     	        for(int j = 0; j < grid.length; j++) {
-    	        	
+    	        	zelle.cell (i,j);
     	        }
     	     
      }
