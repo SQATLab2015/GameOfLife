bec17e431d096f20e78268123f37f3e838cbf0c7
somename <someemail>
2015-10-08 13:30:38 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -1,12 +1,23 @@
 package org.unioulu.tol.sqatlab.gameoflife.test;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.unioulu.tol.sqatlab.gameoflife.Cell;
 import org.unioulu.tol.sqatlab.gameoflife.Grid;
 
 public class TestGrid {
 	
 	Grid grid;
+	
+	
+	@Before
+	public void init()
+	{
+		grid = new Grid();
+	}
+	
 
 	@Test
 	public void testCreateGrid() 
@@ -17,6 +28,12 @@ public class TestGrid {
 
 		assertEquals(5, actual);
 
+	}
+	
+	@Test
+	public void testGetStatusOfCell()
+	{
+		
 	}
 
 }
