ef33de71dd416a63c0075c008556561f9bb447eb
somename <someemail>
2015-10-08 13:23:46 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -11,18 +11,25 @@ public class TestGrid {
 	public void testIsGridSameAmountColumsAndRows() {
 		//getRows x GetColumns == getRows x GetRows
 		//compares 8 to set gridSize in Grid.java
-		 assertEquals(7,Grid.getArray());
+		 assertEquals(8,Grid.getArray());
 		
 		
 	}
 	
 	@Test
 	public void testDoesArrayOnlyIncludeAliveorDeadMarks(){
-		//The state of the grid is called “Life” and shall be represented as a
-		//string, where dead cells are represented with a “-” (dash) and alive
-		//cells are represented with a “*” (star) character.
+	//does array only include * and - 
+		
+	  
+		
+		assertEquals(true,getMarks());
 		
 		
+	}
+
+	private Object getMarks() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 }
