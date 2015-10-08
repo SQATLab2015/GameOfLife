0ddc08ff077e901e5d1c6df4a34caf342df9a6fc
somename <someemail>
2015-10-08 12:58:46 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -2,16 +2,16 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Grid {
 	//GRID X * Y 
-	int gridSize=8;
+	static int gridSize=8;
 	
-	public int[][]array = new int [gridSize][gridSize];
+	public static int[][]array = new int [gridSize][gridSize];
 
 	
 	
 	
-	public int getArray(){
-		int a= array.length;
-		return a;
+public static int getArray(){
+	 int a =	array.length;
+	 return a;
 	}
 
 }
