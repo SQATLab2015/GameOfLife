0587a1d4219639178caa95f01b0444d4072f841d
somename <someemail>
2015-10-08 12:07:10 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -1,5 +1,23 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Cell {
+	
+	public boolean status;
+	
+	public Cell (status)
+	
+	public boolean cellIsDead() {
+		status = false;
+		return status;
+	} 
+	
+	public boolean cellIsAlive() {
+		status = true;
+		return status;
+	}
+		
+		
+	
+	
 
 }
