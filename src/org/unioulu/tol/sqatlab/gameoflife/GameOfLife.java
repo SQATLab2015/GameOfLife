d5f0c34780e56f193cb4e715dc9faded2daa784b
somename <someemail>
2015-10-08 13:47:45 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
@@ -15,7 +15,8 @@ public class GameOfLife {
 			int w = gameGrid.getWidth();
 			int h = gameGrid.getHeight();
 			
-			
+			//Turns out all cells in the grid are only * (alive) or - (dead)
+			//Ran out of time for the excercises, so oh well.
 			for (int i = 0; i < w; i++) {
 				for (int b = 0; b < h; b++) {
 					print(gameGrid.getCellStatus(i,b));
