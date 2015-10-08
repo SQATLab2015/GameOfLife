bb14dcf6d2ace33bf698046c4da709b6f729655d
somename <someemail>
2015-10-08 13:08:59 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,5 +1,6 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -8,16 +9,17 @@ public class Grid {
 	public int rowAmount;
 	public int columnAmount;
 	
-	public Cell[] cells = new Cell[columnAmount];
-	//public List<Cell> cells = null;
-	public List<List<Cell>> allCells = null;
+	
+	
+	public List<Cell> cellArray = null;
+	public List<Cell[]> allCells = null;
 	
 	
 	public int createGrid(int rowAmount)
 	{
 		for(int i = 0; i<rowAmount; i++) 
 		{
-			allCells.add(cells);
+			allCells.add(cellArray);
 		}
 		
 		return allCells.size();
