1f8592dbc7541bdc4fec9de6c71d901cf9724239
somename <someemail>
2015-10-08 13:25:40 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestCell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestCell.java
@@ -3,7 +3,6 @@ package org.unioulu.tol.sqatlab.gameoflife.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import java.util.Arrays;
 
 public class TestCell {
 
@@ -12,23 +11,6 @@ public class TestCell {
 	//	Cell cell = new Cell("Alive");
 	//	cell.update();
 	//	assertEquals.("Dead", cell.getState());
-
-		// Fake 3x3 grid
-		String[] gridRow1 = new String[] {"-", "-", "-"};
-		String[] gridRow2 = new String[] {"-", "-", "-"};
-		String[] gridRow3 = new String[] {"-", "-", "-"};
-		String row1ForPrint = Arrays.toString(gridRow1);
-		row1ForPrint = row1ForPrint.replace(",", "");
-		String row2ForPrint = Arrays.toString(gridRow2);
-		row2ForPrint = row2ForPrint.replace(",", "");
-		String row3ForPrint = Arrays.toString(gridRow3);
-		row3ForPrint = row3ForPrint.replace(",", "");
-
-		System.out.println(row1ForPrint);
-		System.out.println(row2ForPrint);
-		System.out.println(row3ForPrint);
-		
-		
 	}
 
 }
--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -1,25 +1,31 @@
 package org.unioulu.tol.sqatlab.gameoflife.test;
 
 import static org.junit.Assert.*;
-
 import java.util.Arrays;
 
 import org.junit.Test;
 
 public class TestGrid {
 
-	// Fake 3x3 grid
-			String[] gridRow1 = new String[] {"-", "-", "-"};
-			String[] gridRow2 = new String[] {"-", "-", "-"};
-			String[] gridRow3 = new String[] {"-", "-", "-"};
-			String row1ForPrint = Arrays.toString(gridRow1);
-			row1ForPrint = row1ForPrint.replace(",", "");
-			String row2ForPrint = Arrays.toString(gridRow2);
-			row2ForPrint = row2ForPrint.replace(",", "");
-			String row3ForPrint = Arrays.toString(gridRow3);
-			row3ForPrint = row3ForPrint.replace(",", "");
+	@Test
+	public void testGrid() {
 
-			System.out.println(row1ForPrint);
-			System.out.println(row2ForPrint);
-			System.out.println(row3ForPrint);
+
+		// Fake 3x3 grid
+		String[] gridRow1 = new String[] {"-", "-", "-"};
+		String[] gridRow2 = new String[] {"-", "-", "-"};
+		String[] gridRow3 = new String[] {"-", "-", "-"};
+		String row1ForPrint = Arrays.toString(gridRow1);
+		row1ForPrint = row1ForPrint.replace(",", "");
+		String row2ForPrint = Arrays.toString(gridRow2);
+		row2ForPrint = row2ForPrint.replace(",", "");
+		String row3ForPrint = Arrays.toString(gridRow3);
+		row3ForPrint = row3ForPrint.replace(",", "");
+
+		System.out.println(row1ForPrint);
+		System.out.println(row2ForPrint);
+		System.out.println(row3ForPrint);
+		
+		
+	}
 }
