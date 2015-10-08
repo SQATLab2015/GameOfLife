f07d2447a8c3927d597abbb056b22af061230eaf
somename <someemail>
2015-10-08 13:23:43 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -1,12 +1,25 @@
 package org.unioulu.tol.sqatlab.gameoflife.test;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class TestGrid {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	// Fake 3x3 grid
+			String[] gridRow1 = new String[] {"-", "-", "-"};
+			String[] gridRow2 = new String[] {"-", "-", "-"};
+			String[] gridRow3 = new String[] {"-", "-", "-"};
+			String row1ForPrint = Arrays.toString(gridRow1);
+			row1ForPrint = row1ForPrint.replace(",", "");
+			String row2ForPrint = Arrays.toString(gridRow2);
+			row2ForPrint = row2ForPrint.replace(",", "");
+			String row3ForPrint = Arrays.toString(gridRow3);
+			row3ForPrint = row3ForPrint.replace(",", "");
+
+			System.out.println(row1ForPrint);
+			System.out.println(row2ForPrint);
+			System.out.println(row3ForPrint);
 }
