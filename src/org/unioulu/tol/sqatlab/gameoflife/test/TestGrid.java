a5efd85e2f6c8dbfa262e3d9ed53d3b23f2064cf
somename <someemail>
2015-10-08 12:59:44 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -11,7 +11,7 @@ public class TestGrid {
 	public void testIsGridSameAmountColumsAndRows() {
 		//getRows x GetColumns == getRows x GetRows
 		//int a= array.length();	
-		 assertEquals(9,Grid.getArray());
+		 assertEquals(8,Grid.getArray());
 		
 		
 	}
