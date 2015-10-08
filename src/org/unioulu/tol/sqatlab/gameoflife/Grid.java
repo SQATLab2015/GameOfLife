1c7bc8c735ecef03230e2357de689a0aeb1f01f1
somename <someemail>
2015-10-08 13:20:42 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,5 +1,15 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Grid {
-
+     int [][] grid = new int [9][9];
+     
+     public void start() {
+    	 for(int i = 0; i < grid.length; i++)
+    	        for(int j = 0; j < grid.length; j++) {
+    	        	
+    	        }
+    	     
+     }
+       
+       
 }
