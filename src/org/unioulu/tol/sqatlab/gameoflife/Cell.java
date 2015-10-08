95d9c7a80e40f75345db6c9075c33fba49ea6380
somename <someemail>
2015-10-08 12:49:25 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -3,7 +3,15 @@ package org.unioulu.tol.sqatlab.gameoflife;
 public class Cell {
 	
 	public boolean status;
-	
+	public int neighbours;
+		
+	public Cell(int neighbours) {
+		if (neighbours == 0) {
+			setStatus(false);
+		}
+			
+		
+	}
 	
 	public boolean cellIsDead() {
 		status = false;
@@ -13,6 +21,20 @@ public class Cell {
 	public boolean cellIsAlive() {
 		status = true;
 		return status;
+	}
+	
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	public void setNeighbours(int neighbours) {
+		this.neighbours = neighbours;
+		
+	}
+	
+	public boolean getStatus (boolean status) {
+		return status;
+		
 	}
 	
 	}
