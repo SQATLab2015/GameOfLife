159e719bb16248876f866e8d167632df6e066746
somename <someemail>
2015-10-08 12:01:40 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -2,7 +2,7 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Cell {
 
-	public static final CELL_STATE_DEAD = 0;
+	public static final int CELL_STATE_DEAD = 0;
 	public static final int CELL_STATE_ALIVE = 1;
 	
 	public int getState() {
