984a14b46960691f81a8803753dd9380712f5bc1
somename <someemail>
2015-10-08 12:53:36 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -9,13 +9,13 @@ public class Grid {
 	}
 	
 	public Grid(int width, int height) {
-		gridArray = new int[width][height];
+		gridArray = new Cell[width][height];
 		int i = 0;
 		int b = 0;
 		
 		while ( i < width) {
 			while ( b < height ) {
-				gridArray[i][b] = 0;
+				gridArray[i][b] = new Cell();
 				b++;
 			}
 			i++;
@@ -30,6 +30,10 @@ public class Grid {
 	
 	public Cell getCell (int x, int y) {
 		return gridArray[x][y];
+	}
+	
+	public String getCellStatus Status ( int x, int y ) {
+		return getCell(x,y).getStatus();
 	}
 	
 }
