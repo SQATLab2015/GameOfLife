d2af665b723e94fe55c61aca76a2fcacdc370ee0
somename <someemail>
2015-10-08 12:41:02 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -1,5 +1,13 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Cell {
+	public int collum;
+	public int row;
+	public int neighbor;
 
+	public Cell(int collum, int row) {
+		this.collum = collum;
+		this.row = row;
+		neighbor = 0;
+	}
 }
