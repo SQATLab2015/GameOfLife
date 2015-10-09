52f47d32c345abb143e6319c805a35323acf2ab5
somename <someemail>
2015-10-09 12:41:43 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -8,13 +8,28 @@ import org.unioulu.tol.sqatlab.gameoflife.Grid;
 public class TestGrid {
 
 	@Test
-	public void test() {
+	public void testSingleCellOnGridHasNoNeighbors() {
+		//arrange
 		Grid grid = new Grid();
 		Cell cell = new Cell(0,0);
-		
+		//act
+		grid.addCell(cell);
 		int numNeighbors = grid.getNeighbors(cell);
-	
+		//assert
 		assertEquals(0, numNeighbors);
 	}
 
+	@Test
+	public void testSingleCellOnGridHasNoNeighbors() {
+		//arrange
+		Grid grid = new Grid();
+		Cell cell1 = new Cell(0,0);
+		Cell cell2 = new Cell(0,1);
+		//act
+		grid.addCell(cell1);
+		grid.addCell(cell2);
+		int numNeighbors = grid.getNeighbors(cell1);
+		//assert
+		assertEquals(1, numNeighbors);
+	}
 }
