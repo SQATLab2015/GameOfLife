00768b6e341e7ca6da73b5187c70894b79fa9aa1
somename <someemail>
2015-10-09 12:38:16 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -3,9 +3,16 @@ package org.unioulu.tol.sqatlab.gameoflife;
 public class Cell {
 
 	String state;
+	int x;
+	int y;
 	
 	public Cell(String initialState) {
 		this.state = initialState;
+	}
+
+	public Cell(int i, int j) {
+		this.x = i;
+		this.y = j;
 	}
 
 	public void nextIteration(int numOfLiveCells) {
