84ce07457cfbe26fcc4cc063ba60127625c9a9c3
somename <someemail>
2015-10-09 12:58:50 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -2,6 +2,37 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Cell {
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cell other = (Cell) obj;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
 	String state;
 	int x;
 	int y;
--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,15 +1,18 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Grid {
 
+	private Set<Cell> cells = new HashSet<>();
+	
 	public int getNeighbors(Cell cell) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public void addCell(Cell cell) {
-		// TODO Auto-generated method stub
-		
+		cells.add(cell);
 	}
 
 }
