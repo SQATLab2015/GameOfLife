74021d3033e20bfdd3feac0522499f12336b0525
somename <someemail>
2015-10-09 12:24:45 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -2,4 +2,24 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Cell {
 
+	private String state;
+	
+	public Cell(){
+		
+	}
+
+	public Cell(String initialState) {
+		this.state = initialState;
+	}
+
+	public void nextIteration() {
+		
+		
+	}
+
+	public String getState() {
+		// TODO Auto-generated method stub
+		return this.state;
+	}
+	
 }
