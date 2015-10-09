7cb41cdcc54024fed23cf34fc6143b256c1af94b
somename <someemail>
2015-10-09 12:20:30 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestCell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestCell.java
@@ -3,12 +3,19 @@ package org.unioulu.tol.sqatlab.gameoflife.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqatlab.gameoflife.Cell;
 
 public class TestCell {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLiveCellWithNoNeighborsShouldDie() {
+		//arrange
+		Cell cell = new Cell("Alive");
+		//
+		cell.nextIteration();
+		//
+		//assert
+		assertEquals("Dead", cell.getState());
 	}
 
 }
