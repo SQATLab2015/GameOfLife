d5a97dd2fd2441684845ab8e0dc42ea154de436c
somename <someemail>
2015-10-09 12:38:19 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -2,12 +2,19 @@ package org.unioulu.tol.sqatlab.gameoflife.test;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.unioulu.tol.sqatlab.gameoflife.Cell;
+import org.unioulu.tol.sqatlab.gameoflife.Grid;
 
 public class TestGrid {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Grid grid = new Grid();
+		Cell cell = new Cell(0,0);
+		
+		int numNeighbors = grid.getNeighbors(cell);
+	
+		assertEquals(0, numNeighbors);
 	}
 
 }
