0d789a1efa1e8c90980b8c472b758dde81deb47b
somename <someemail>
2015-10-09 12:42:19 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -20,7 +20,7 @@ public class TestGrid {
 	}
 
 	@Test
-	public void testSingleCellOnGridHasNoNeighbors() {
+	public void testSingleCellOnGridHasOneNeighbor() {
 		//arrange
 		Grid grid = new Grid();
 		Cell cell1 = new Cell(0,0);
