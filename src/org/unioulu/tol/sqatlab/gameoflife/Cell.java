4a4ec5aa24a0e0f95211bf09f2a7586276c1c843
somename <someemail>
2015-10-09 12:20:34 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -2,4 +2,18 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Cell {
 
+	public Cell(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void nextIteration() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
