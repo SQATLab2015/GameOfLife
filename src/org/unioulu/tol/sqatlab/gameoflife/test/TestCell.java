b946d81b781745c16e21a252a9d41abe05321f41
somename <someemail>
2015-10-09 23:24:26 +0800

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestCell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestCell.java
@@ -3,13 +3,14 @@ package org.unioulu.tol.sqatlab.gameoflife.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqatlab.gameoflife.Cell;
 
 public class TestCell {
 
 	@Test
 	public void testCellState() {
 		Cell c = new Cell("*");
-		assertEquals("*", c.state);
+		assertEquals("-", c.getState());
 	}
 
 }
