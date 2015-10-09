b8ec05afa5ba852c40b4dc448963a1cdcd3b532f
somename <someemail>
2015-10-09 12:44:07 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -2,4 +2,20 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Grid {
 
+	public Grid(){
+		
+	}
+
+	public int getNeighbors(Cell cell) {
+		
+		return 0;
+	}
+
+	public void addCell(Cell cell) {
+		
+		
+	}
+	
+	
+	
 }
