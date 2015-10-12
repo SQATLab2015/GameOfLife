ac539ff3cf617fad5af66c541ea47ba9782f7aba
somename <someemail>
2015-10-12 17:18:26 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -34,6 +34,7 @@ public class Cell {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
 		result = prime * result + x;
 		result = prime * result + y;
 		return result;
@@ -48,12 +49,19 @@ public class Cell {
 		if (getClass() != obj.getClass())
 			return false;
 		Cell other = (Cell) obj;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
 		if (x != other.x)
 			return false;
 		if (y != other.y)
 			return false;
 		return true;
 	}
+
+
 	
 	
 
