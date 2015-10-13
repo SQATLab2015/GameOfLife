910eb37af3a8298fcbf2b25ee1df38dbb10cd56e
somename <someemail>
2015-10-14 03:39:15 +0800

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -7,14 +7,25 @@ import org.unioulu.tol.sqatlab.gameoflife.Grid;
 public class TestGrid {
 
 	@Test
-	public void testCreateGrid() {
+	public void testCreate() {
 		Grid g = new Grid(5);
 		g.getGrid()[1][1].setState("-");
 		assertEquals("-", g.getGrid()[1][1].getState());
 	}
 	
 	@Test
-	public void testDisplayGrid() {
+	public void testDisplay() {
+		Grid g = new Grid(2);
+		g.getGrid()[0][0].setState("-");
+		g.getGrid()[0][1].setState("-");
+		g.getGrid()[1][0].setState("-");
+		g.getGrid()[1][1].setState("-");
+		g.display();
+		//assertEquals("-", g.getGrid()[1][1].getState());
+	}
+	
+	@Test
+	public void testCount() {
 		Grid g = new Grid(2);
 		g.getGrid()[0][0].setState("-");
 		g.getGrid()[0][1].setState("-");
