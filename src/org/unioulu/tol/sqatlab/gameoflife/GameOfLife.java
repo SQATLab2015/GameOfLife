2139182f47a527ec9755edcbe5063dd9f247483f
somename <someemail>
2015-10-14 04:36:32 +0800

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
@@ -5,7 +5,7 @@ public class GameOfLife {
 		Grid g = new Grid(2);
 		g.getGrid()[0][0].setState("*");
 		g.getGrid()[0][1].setState("*");
-		g.getGrid()[1][0].setState("*");
+		g.getGrid()[1][0].setState("-");
 		g.getGrid()[1][1].setState("*");
 		g.display();
 		g.tick();
