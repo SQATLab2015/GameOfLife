6dad76b3962597ffd3bdcac5c7dca1f72362be57
somename <someemail>
2015-10-13 20:27:35 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/CustomLifeException.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/CustomLifeException.java
@@ -1,6 +1,12 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
 public class CustomLifeException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
 	public CustomLifeException(String msg){
 	      super(msg);
 	  }
