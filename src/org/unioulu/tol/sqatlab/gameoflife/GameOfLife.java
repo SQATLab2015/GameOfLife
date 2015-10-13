8b91f9bed0607209754098ec9cf55536be0eea7f
somename <someemail>
2015-10-14 04:36:05 +0800

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
@@ -2,16 +2,11 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class GameOfLife {
 	public static void main(String[] args) {
-		Grid g = new Grid(3);
+		Grid g = new Grid(2);
 		g.getGrid()[0][0].setState("*");
 		g.getGrid()[0][1].setState("*");
-		g.getGrid()[0][2].setState("*");
 		g.getGrid()[1][0].setState("*");
-		g.getGrid()[1][1].setState("-");
-		g.getGrid()[1][2].setState("*");
-		g.getGrid()[2][0].setState("*");
-		g.getGrid()[2][1].setState("*");
-		g.getGrid()[2][2].setState("*");
+		g.getGrid()[1][1].setState("*");
 		g.display();
 		g.tick();
 		g.display();
