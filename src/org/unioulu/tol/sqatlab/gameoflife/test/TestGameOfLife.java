f25df9261f5a7cd01d154dfe1669c89b6b3195e5
somename <someemail>
2015-10-13 22:28:23 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGameOfLife.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGameOfLife.java
@@ -12,44 +12,44 @@ public class TestGameOfLife {
 	String[] nineStates = {"Alive", "Alive", "Alive", "Dead", "Dead", "Dead", "Dead", "Dead", "Dead"};
 	
 	
-//	@Test
-//	public void testSetupGameNineCells_ReturnNine() {
-//		gameOfLife = new GameOfLife(3);
-//		gameOfLife.setupGame(gameOfLife.createRandomStates(gameOfLife.getNumberOfCells()));
-//		int numberOfCells = gameOfLife.grid.cells.size();
-//		
-//		assertEquals(9, numberOfCells);
-//	}
-//	
-//	@Test
-//	public void testSetupGameOneCell_ReturnOne() {
-//		gameOfLife = new GameOfLife(1);
-//		gameOfLife.setupGame(gameOfLife.createRandomStates(gameOfLife.getNumberOfCells()));
-//		int numberOfCells = gameOfLife.grid.cells.size();
-//		
-//		assertEquals(1, numberOfCells);
-//	}
-//	
-//	@Test
-//	public void testCreateRandomStates(){
-//		gameOfLife = new GameOfLife(2);
-//		String[] states = gameOfLife.createRandomStates(gameOfLife.getNumberOfCells());
-//		
-//		assertEquals(4, states.length);
-//		assertEquals(true, states[0].matches("Dead|Alive"));
-//		assertEquals(true, states[1].matches("Dead|Alive"));
-//		assertEquals(true, states[2].matches("Dead|Alive"));
-//		assertEquals(true, states[3].matches("Dead|Alive"));
-//	}
-//	
-//	
-//	@Test
-//	public void testPrintCurrentGameState() throws CustomLifeException{
-//		gameOfLife = new GameOfLife(3);
-//		gameOfLife.setupGame(nineStates);
-//		
-//		assertEquals("***\n---\n---\n", gameOfLife.printCurrentGameState());
-//	}
+	@Test
+	public void testSetupGameNineCells_ReturnNine() {
+		gameOfLife = new GameOfLife(3);
+		gameOfLife.setupGame(gameOfLife.createRandomStates(gameOfLife.getNumberOfCells()));
+		int numberOfCells = gameOfLife.grid.cells.size();
+		
+		assertEquals(9, numberOfCells);
+	}
+	
+	@Test
+	public void testSetupGameOneCell_ReturnOne() {
+		gameOfLife = new GameOfLife(1);
+		gameOfLife.setupGame(gameOfLife.createRandomStates(gameOfLife.getNumberOfCells()));
+		int numberOfCells = gameOfLife.grid.cells.size();
+		
+		assertEquals(1, numberOfCells);
+	}
+	
+	@Test
+	public void testCreateRandomStates(){
+		gameOfLife = new GameOfLife(2);
+		String[] states = gameOfLife.createRandomStates(gameOfLife.getNumberOfCells());
+		
+		assertEquals(4, states.length);
+		assertEquals(true, states[0].matches("Dead|Alive"));
+		assertEquals(true, states[1].matches("Dead|Alive"));
+		assertEquals(true, states[2].matches("Dead|Alive"));
+		assertEquals(true, states[3].matches("Dead|Alive"));
+	}
+	
+	
+	@Test
+	public void testPrintCurrentGameState() throws CustomLifeException{
+		gameOfLife = new GameOfLife(3);
+		gameOfLife.setupGame(nineStates);
+		
+		assertEquals("***\n---\n---\n", gameOfLife.printCurrentGameState());
+	}
 	
 	@Test
 	public void testDoOneRound() throws CustomLifeException{
