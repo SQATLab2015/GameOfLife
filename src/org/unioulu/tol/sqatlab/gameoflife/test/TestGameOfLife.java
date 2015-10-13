a2537d993ae458a015c9f89c6a640007d87e2050
somename <someemail>
2015-10-13 20:32:29 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGameOfLife.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGameOfLife.java
@@ -3,13 +3,17 @@ package org.unioulu.tol.sqatlab.gameoflife.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqatlab.gameoflife.GameOfLife;
 
 public class TestGameOfLife {
-
+	GameOfLife gameOfLife;
 	
 	@Test
 	public void testSetupGame() {
-		fail("Not yet implemented");
+		gameOfLife = new GameOfLife(3);
+		int numberOfCells = gameOfLife.grid.cells.size();
+		
+		assertEquals();
 	}
 	
 }
